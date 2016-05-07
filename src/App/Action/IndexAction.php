8cf5c46ac802e82ec00cad1efbcb2aeae8e31df0
Sergey <drakulitka@mail.ru>
2016-05-07 14:51:25 +0300

    555

---
+++ src/App/Action/IndexAction.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Action;
+
+use Doctrine\ORM\EntityManager;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Zend\Diactoros\Response\HtmlResponse;
+use Zend\Expressive\Template\TemplateRendererInterface;
+
+class IndexAction
+{
+    private $em;
+
+    private $template;
+
+    public function __construct(EntityManager $em, TemplateRendererInterface $template)
+    {
+        $this->em = $em;
+        $this->template = $template;
+    }
+
+    public function __invoke(ServerRequestInterface $request, ResponseInterface $response, callable $next = null)
+    {
+        $userRepository = $this->em->getRepository('App\Entity\Settings');
+        $settings = $userRepository->findAll();
+
+        return new HtmlResponse($this->template->render('app::index', [$settings]));
+    }
+}
